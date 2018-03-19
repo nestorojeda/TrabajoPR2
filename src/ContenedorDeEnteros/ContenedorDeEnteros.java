1139c0a5d994ee204d2a1eb05bd96f9e183bd2e9
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2018-03-19 10:13:03 +0000

    Añadido elementos() a la interface

--- src/ContenedorDeEnteros/ContenedorDeEnteros.java
+++ src/ContenedorDeEnteros/ContenedorDeEnteros.java
@@ -1,9 +1,11 @@
 package ContenedorDeEnteros;
 
-interface ContenedorDeEnteros {
+public interface ContenedorDeEnteros {
     public boolean insertar(int valor);
     public boolean extraer(int valor);
     public boolean buscar(int valor);
     public int cardinal();
     public void vaciar();
+    public int[] elementos();
+
 }
